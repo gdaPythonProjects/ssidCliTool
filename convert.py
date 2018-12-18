70141786d645b42dd0bf2b687af1a7a93135df07
lucynao <44733630+lucynao@users.noreply.github.com>
2018-12-18 23:10:12 +0100

    Add files via upload
    Funkcja aplikacji:
    - dla podanych współrzędnych wpisać jakie sieci WIFI znajdują się w pobliżu - skończona
    - dla podanej nazwy sieci wypisać w jakich miejscach taka nazwa występuje - w realizacji

--- convert.py
+++ convert.py
@@ -6,3 +6,14 @@ class Convert:
 		
 	def JsonToDict(self,data):
 		return json.loads(data)
+		
+	def JsonListOfWiFiToListOfWiFiNames(self,jsonData):
+		dictionary = self.JsonToDict(jsonData)
+		listOfNames=[]
+		i=0
+		while i < len(dictionary["results"]):
+			listOfNames.append((dictionary["results"][i]["ssid"]))
+			i=i+1
+		return listOfNames
+		
+		
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import http_helper
 import convert
-
-http_client = http_helper.HTTP_Helper("credentials.txt")
-content=http_client.GetListOfWiFi(54.3545,54.3570,18.6404,18.649)
-
-file = open("content.html","w")
-file.write(content)
-
-converter = convert.Convert()
-jsonDict = converter.JsonToDict(content)
+import sys
 
 
+if len(sys.argv)==5:
+	
+	latrange1 = float(sys.argv[1])
+	latrange2 = float(sys.argv[2])
+	longrange1 = float(sys.argv[3])
+	longrange2 = float(sys.argv[4])
+	
+	http_client = http_helper.HTTP_Helper("credentials.txt")
+	content=http_client.GetListOfWiFi(latrange1,latrange2,longrange1,longrange2)	
+
+	converter = convert.Convert()
+	listOfWiFiNames = converter.JsonListOfWiFiToListOfWiFiNames(content)
+
+	for element in listOfWiFiNames:
+		print(element)
+			
+elif len(sys.argv)==2:
+
+	print("funcja chwilowo niedostepna- w trakcie budowy")
+	
+else:
+	print("podano nieprawidlowa liczbe parametrow")
+	print()
+	print("program oferuje 2 opcje")
+	print()
+	print("w pierwszej opcji wyswielta nazwy sieci Wi-Fi na podstawie wprowadzonych wspolrzednych geograficznych : latrange1 latrange2 longrange1 longrange2 ")
+	print()
+	print("w drugiej opcji wyswietla lokalizacje sieci Wi-Fi na podstawie wprowadzonej nazwy sieci")
